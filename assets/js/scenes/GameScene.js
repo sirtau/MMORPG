acde81f1951c68e7d3177e107bac46ad5f7cc722
sirtau <contact@hamidmousa.com>
2021-08-17 18:39:46 +1000

    Added Object Pooling for Chests
    Chests will now be made inactive and recycled instead of destroyed.
    Improves runtime efficiency.

--- assets/js/scenes/GameScene.js
+++ assets/js/scenes/GameScene.js
@@ -9,26 +9,18 @@ class GameScene extends Phaser.Scene {
 
     init() {
         this.scene.launch('UI')
+        this.score = 0
     }
 
     create() {
         this.createAudio()
         this.createWalls()
         this.createPlayer()
-        
-
-
-    
-
-
-    
         this.createInput()
         this.createChests()
+        this.spawnChest()
         this.addCollisions()
-    
-
-
-    }
+        }
 
     update() {
     this.player.update(this.cursors)
@@ -45,11 +37,31 @@ class GameScene extends Phaser.Scene {
     }
 
     createChests() {
-        this.chest = new Chest(this, 300, 300, 'items', 0)
+        this.chests = this.physics.add.group()
+        this.maxNumberOfChests = 3
+        this.chestPositions = [[100, 100], [200, 200], [400, 400], [500, 500]]
+        for (let i = 0; i < this.maxNumberOfChests; i++) {
+            this.spawnChest()
+        }
+    }
+
+    spawnChest() {
+        const location = this.chestPositions[Math.floor(Math.random() * this.chestPositions.length)]
+        let chest = this.chests.getFirstDead()
+
+        if (!chest) {
+            let chest = new Chest(this, location[0], location[1], 'items', 0)
+            this.chests.add(chest)
+        } else {
+            chest.setPosition(location[0], location[1])
+            chest.makeActive()
+        }
+
+
     }
 
     createWalls() {
-        this.wall = this.physics.add.image(100, 100, 'button1')
+        this.wall = this.physics.add.image(300, 100, 'button1')
             .setOrigin(0)
             .setImmovable()
     }
@@ -60,14 +72,18 @@ class GameScene extends Phaser.Scene {
 
     addCollisions() {
         this.physics.add.collider(this.player, this.wall)
-        this.physics.add.overlap(this.player, this.chest, this.collectChest, null, this )
+        this.physics.add.overlap(this.player, this.chests, this.collectChest, null, this )
     }
 
     collectChest(player, chest) {
         this.goldPickupAudio.play()
-        this.events.emit('updateScore', chest.coins)
-        chest.destroy()
+        this.score += chest.coins
+        this.events.emit('updateScore', this.score)
+        chest.makeInactive()
+        this.time.delayedCall(1000, this.spawnChest, [], this)
     }
+
+
 }
 
 
