fa4d98a309ae04ec40a9aebf6d957a9dff2748f0
sirtau <contact@hamidmousa.com>
2021-08-20 13:46:18 +1000

    Added weapon, animates/flips based on player character.
    Added logic to displace, flip and animate the Weapon.
    Weapon is added to "this", being the PlayerContainer.
    
    TODO - Work out weapon placement - not evenly distanced relative to the custom hitbox. Tutorial doesn't adjust the character hitbox, at least not yet.

--- assets/js/classes/player/PlayerContainer.js
+++ assets/js/classes/player/PlayerContainer.js
@@ -1,20 +1,43 @@
 import Phaser from 'phaser'
 import Player from './Player.JS'
 
+const Direction = {
+    RIGHT: 'RIGHT',
+    LEFT: 'LEFT',
+    UP: 'UP',
+    DOWNL: 'DOWN',
+}
+
 class PlayerContainer extends Phaser.GameObjects.Container {
     constructor(scene, x, y, key, frame) {
         super(scene, x, y)
         this.scene = scene
         this.velocity = 360
-        this.setSize(64, 64)
-        this.scene.physics.world.enable(this)
+        this.currentDirection = Direction.RIGHT
+        this.playerAttacking = false
+        this.flipX = true
+        this.swordHit = false
 
+        
+
+        this.setSize(32, 32)
+        // physics need to be enabled to create the Body, which can then be modified with .body.<method>
+        this.scene.physics.world.enable(this)
         this.body.setCollideWorldBounds(true)
+        this.body.setOffset(0, 18)
+        
         this.scene.add.existing(this)
         this.scene.cameras.main.startFollow(this)
 
         this.player = new Player(this.scene, 0, 0, key, frame)
         this.add(this.player)
+
+        this.weapon = this.scene.add.image(40, 0, 'items', 4)
+        this.scene.add.existing(this.weapon)
+        this.weapon.setScale(1.5)
+        this.scene.physics.world.enable(this.weapon)
+        this.add(this.weapon)
+        this.weapon.alpha = 0
     }
 
     update(cursors) {
@@ -22,15 +45,55 @@ class PlayerContainer extends Phaser.GameObjects.Container {
 
         if (cursors.left.isDown) {
             this.body.setVelocityX(-160)
+            this.currentDirection = Direction.LEFT
+            this.weapon.setPosition(-40, 0)
         } else if (cursors.right.isDown) {
             this.body.setVelocityX(160)
+            this.currentDirection = Direction.DOWN
+            this.weapon.setPosition(40, 0)
         } 
     
         if (cursors.up.isDown) {
             this.body.setVelocityY(-160)
+            this.currentDirection = Direction.UP
+            this.weapon.setPosition(0, -40)
         } else if (cursors.down.isDown) {
             this.body.setVelocityY(160)
+            this.currentDirection = Direction.DOWN
+            this.weapon.setPosition(0, 40)
         } 
+        
+        if (Phaser.Input.Keyboard.JustDown(cursors.space) && !this.playerAttacking) {
+            console.log('attack')
+            this.weapon.alpha = 1
+            this.playerAttacking = true
+            this.scene.time.delayedCall(150, () => {
+                this.weapon.alpha = 0
+                this.playerAttacking = false
+                this.swordHit = false
+            }, [], this)
+        }
+
+        if (this.playerAttacking) {
+            if (this.weapon.fipX) {
+                this.weapon.angle -= 20
+            } else {
+                this.weapon.angle += 20
+            }
+
+        } else {
+            if (this.currentDirection === Direction.DOWN) {
+                this.weapon.setAngle(-270)
+            } else if (this.currentDirection === Direction.UP) {
+                this.weapon.setAngle(-90)
+            } else {
+                this.weapon.setAngle(0)
+            }
+            this.weapon.flipX = false
+            if (this.currentDirection === Direction.LEFT) {
+                this.weapon.flipX = true
+            }
+         }
     }
 
 }
